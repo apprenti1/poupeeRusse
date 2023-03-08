ffc14d0753a6d15be1e1a3d8cf7836028f09159f
AIT-BOUKHA_El <elias.aitboukha@gmail.com>
2023-03-08 12:10:58 +0100

    upload poupee class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,2 +1,14 @@
-package main;public class Main {
+package main;
+import poupee.Poupee;
+
+class Main {
+    public static void main(String[] args) {
+        Poupee poupee1 = new Poupee(1);
+        Poupee poupee2 = new Poupee(2);
+        Poupee poupee3 = new Poupee(3);
+        System.out.println(poupee1.placerDans(poupee2));
+        System.out.println(poupee1.ouvrir());
+        System.out.println(poupee1.isOpened());
+        System.out.println();
+    }
 }
--- src/poupee/Poupee.java
+++ src/poupee/Poupee.java
@@ -1,2 +1,59 @@
-package poupee;public class Poupee {
+package poupee;
+
+public class Poupee {
+    private int taille = 1;
+    private Poupee poupeeEnfante;
+    private Poupee poupeeParante;
+    private boolean opened = false;
+
+
+    public Poupee(int taille){
+        if (taille>0) {
+            this.taille = taille;
+        }
+    }
+    public boolean ouvrir(){
+        if (poupeeParante == null){
+            this.opened = true;
+            return true;
+        } else if (poupeeParante.opened) {
+            this.opened = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean fermer(){
+        if (poupeeEnfante == null){
+            this.opened = false;
+            return true;
+        } else if (!poupeeEnfante.opened) {
+            this.opened = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean placerDans(Poupee p){
+        if (this.opened == false && this.poupeeParante == null && p.opened && p.poupeeEnfante == null && this.taille < p.taille){
+            this.poupeeParante = p;
+            p.poupeeEnfante = this;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean sortirDe(Poupee p){
+        if (this.opened == false && this.poupeeParante == p && p.opened){
+            this.poupeeParante = null;
+            p.poupeeEnfante = null;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean isOpened() {return opened;}
 }
